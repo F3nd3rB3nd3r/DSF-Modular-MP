75cf6ccef715109e90f0bd91a02de53360154f2f
F3nd3rB3nd3r <>
2024-02-10 16:43:32 +0100

    switch from loadfile to user_open and add error handling

--- Common/UserLuaScripts/customMPcontent.lua
+++ Common/UserLuaScripts/customMPcontent.lua
@@ -1,10 +1,25 @@
-local function isPlayerInPartyBus ()
+module("customMPcontent", package.seeall)
+
+local folderPrefix = "COM:UserLuascripts\\customMPcontent\\"
+
+local function isPlayerInPartyBus()
 	if PartyBusManager.IsInPartyBus() then
-		user_open("customMpcontent\\filelist.lua")
+		loadFile("filelist.lua")
 		removeUserUpdateFunction("customMPcontent_checkIsPlayerInPartyBus")
+		print("loaded")
 	end
 end
 
 if Network.isOnlineGame() then
 	addUserUpdateFunction("customMPcontent_checkIsPlayerInPartyBus", isPlayerInPartyBus, 120)
+end
+
+function loadFile(path)
+	local content, readError = loadfile(folderPrefix .. path)
+	if not content then
+		print(readError)
+		PauseMenu.quit()
+	else
+		content()
+	end
 end
