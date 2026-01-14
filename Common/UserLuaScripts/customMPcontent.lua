9619b95475851c8efb25e419bd22b16eaad064b0
F3nd3rB3nd3r <>
2026-01-14 09:29:05 +0100

    add bonus cars to taxi category

--- Common/UserLuaScripts/customMPcontent.lua
+++ Common/UserLuaScripts/customMPcontent.lua
@@ -1,6 +1,6 @@
 module("customMPcontent", package.seeall)
 
-local version = "v1.0"
+local version = "v1.01"
 
 function getVersion()
     return version
@@ -42,3 +42,33 @@ end
 Atlas.JerichoAlleyWayActive(true)
 propSystem.disablePropType("DO_NOT_USE_shutter_A", "DO_NOT_USE_shutter_B", "DO_NOT_USE_Wall_A")
 
+onlineProgressionSystem.onlineVehicleData[36] = {
+    name = "Bonus",
+	new = true,
+	isUplayPack = true,
+    unlocked = true,
+    vehicles = {
+    [1] = 302,
+		[2] = 118,
+		[3] = 298,
+		[4] = 280,
+		[5] = 268,
+		[6] = 267,
+		[7] = 255,
+		[8] = 246,
+		[9] = 226,
+		[10] = 201,
+		[11] = 286,
+		[12] = 268,
+		[13] = 270,
+		[14] = 272,
+		[15] = 282
+	}
+}
+
+for _, vehicleId in ipairs(onlineProgressionSystem.onlineVehicleData[36].vehicles) do
+    vehicleStats[vehicleId].Picture1 = "alfa_romeo_mito"
+    vehicleStats[vehicleId].Picture2 = "alfa_romeo_mito_icon_large"
+    vehicleStats[vehicleId].Picture3 = "alfa_romeo_mito_icon_small"
+    vehicleStats[vehicleId].OnlineGroup = "231209"
+end 
